35331093832d8375dee8a9c8898e9b7a34ee4fce
Ilacoume <ilacoume@irontec.com>
2021-09-22 11:55:20 +0200

    Added tristate versions

---
+++ pripple/src/main/java/com/inlacou/pripple/tristate/TristateRippleFrameLayout.kt
@@ -0,0 +1,54 @@
+package com.inlacou.pripple.tristate
+
+import android.content.Context
+import android.util.AttributeSet
+import com.inlacou.pripple.RippleFrameLayout
+import com.inlacou.pripple.RippleLinearLayout
+import com.inlacou.pripple.RippleableMdl
+
+open class TristateRippleFrameLayout: RippleFrameLayout {
+	constructor(context: Context) : super(context)
+	constructor(context: Context, attrSet: AttributeSet) : super(context, attrSet) { readAttrs(attrSet) }
+	constructor(context: Context, attrSet: AttributeSet, arg: Int) : super(context, attrSet, arg) { readAttrs(attrSet) }
+	
+	var special: Boolean = false
+		set(value) {
+			field = value
+			update()
+		}
+	
+	var normalTheme: RippleableMdl? = null
+	var specialTheme: RippleableMdl? = null
+	var specialDisabledTheme: RippleableMdl? = null
+	var disabledTheme: RippleableMdl? = null
+	
+	override fun setEnabled(enabled: Boolean) {
+		super.setEnabled(enabled)
+		update()
+	}
+	
+	fun update() {
+		when {
+			!isEnabled && special -> {
+				// not enabled and special -> disabled special
+				specialDisabledTheme
+			}
+			!isEnabled && !special -> {
+				// not enabled and not special -> disabled normal
+				disabledTheme
+			}
+			isEnabled && special -> {
+				// enabled and special -> special
+				specialTheme
+			}
+			isEnabled && !special -> {
+				// enabled and not special -> normal
+				normalTheme
+			}
+			else -> null
+		}?.let {
+			applyMdl(it)
+		}
+	}
+	
+}
